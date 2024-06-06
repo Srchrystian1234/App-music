df6277fa97a0fbd9dc79684b1099ae1a1b304947
Srchrystian1234 <95313820+Srchrystian1234@users.noreply.github.com>
2024-06-06 16:49:34 -0400

    fizemdo os play Pause e criamos os Array
    Co-Authored-By: Mauricio  Mathias - devoloper <88565329+MauricioMathiass@users.noreply.github.com>

--- music.js
+++ music.js
@@ -1,14 +1,57 @@
 const songName = window.document.getElementById('song-name');                              
 const song = window.document.getElementById('audio');
-
 const play = window.document.getElementById('play')
+const bandName = window.document.getElementById('band-name');
+const coverDisc = window.document.getElementById('cover')
 
-songName.innerText = 'I Want It That Way';
-
-
-
-function playSong (){
-   song.play(); 
+const IWantItThatWay = {
+    songName: 'I Want It That Way',
+    artist: 'Backstreet boys',
+    file:'Backs'
 }
 
-play.addEventListener('click',playSong);
+const ZumZumZum = {
+    songName: 'zumzumzum',
+    artist: 'Teto raps',
+    file:'Teto'
+}
+const SoldaMeiaNoite = {
+    songName: 'Sol da Meia Noite',
+    artist: 'Rosa de Saron',
+    file:'Rosa'
+}
+
+let isPlaying = false
+
+const playlist = [IWantItThatWay,ZumZumZum,SoldaMeiaNoite]
+let index = 0;
+
+function playSong (){
+   
+        play.querySelector('.bi').classList.remove('bi-play-circle-fill')
+        play.querySelector('.bi').classList.add('bi-pause-circle-fill')
+         song.play();
+         isPlaying = true;
+    
+}
+function pauseSong (){
+   
+    play.querySelector('.bi').classList.remove('bi-pause-circle-fill')
+    play.querySelector('.bi').classList.add('bi-play-circle-fill')
+     song.pause();
+     isPlaying = false;
+     
+
+}
+function PlaypauseDecider(){
+    if(isPlaying === true){
+        pauseSong();
+    }else{
+        playSong();
+    }
+
+}
+
+
+
+play.addEventListener('click',PlaypauseDecider);
