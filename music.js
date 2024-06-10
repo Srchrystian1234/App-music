bfceb0331481b384d61ab5a7c62923cef7894b0c
Srchrystian1234 <95313820+Srchrystian1234@users.noreply.github.com>
2024-06-10 11:43:21 -0400

    commmit da musica

--- music.js
+++ music.js
@@ -3,6 +3,8 @@ const song = window.document.getElementById('audio');
 const play = window.document.getElementById('play')
 const bandName = window.document.getElementById('band-name');
 const coverDisc = window.document.getElementById('cover')
+const next = window.document.getElementById('next')
+const previous = window.document.getElementById('previous')
 
 const IWantItThatWay = {
     songName: 'I Want It That Way',
@@ -48,10 +50,47 @@ function PlaypauseDecider(){
         pauseSong();
     }else{
         playSong();
+        
     }
 
 }
 
+function initializeSong (){
+    coverDisc.src = `src/imagens/${playlist[index].file}.jpg`;
 
+    song.src = `src/Songs/${playlist[index].file}.mp3`;
+    songName.innerHTML = `${playlist[index].songName}`
+    bandName.innerHTML = playlist[index].artist;
+}
 
+function previousSong(){
+    if(index === 0){
+        index = playlist.length -1// quando chegar no -1 , ele vai jogar pro comprimento do array que nesse caso é o numero 2 
+        
+    }else{
+        index--
+       
+    }
+    initializeSong();
+    playSong();
+  
+}
+function nextSong(){
+    if(index === playlist.length -1){
+        index = 0
+        // nesse caso ele so inverter a situacao 
+        
+    }else{
+        index++
+       
+    }
+    initializeSong();
+    playSong();
+  
+}
+initializeSong();
 play.addEventListener('click',PlaypauseDecider);
+
+previous.addEventListener('click',previousSong)
+
+next.addEventListener('click',nextSong)
