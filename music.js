65380bd638847717ec9056b0c16e243951c31266
Srchrystian1234 <95313820+Srchrystian1234@users.noreply.github.com>
2024-06-12 16:43:30 -0400

    enclementei os comentarios
    Co-Authored-By: Mauricio  Mathias - devoloper <88565329+MauricioMathiass@users.noreply.github.com>

--- music.js
+++ music.js
@@ -1,4 +1,4 @@
-//primeira parte de tudo e chamado de acordo com a necessidade 
+//primeira parte de tudo e  fazer aschamadas de acordo com a necessidade 
 
 const songName = window.document.getElementById('song-name');                              
 const song = window.document.getElementById('audio');
@@ -16,8 +16,12 @@ const Repeatbutton = window.document.getElementById('repeat')
 const SongTime = window.document.getElementById('song-time')
 const totalTime = window.document.getElementById('total-time')
 
+
+// criamos alguns objetos (nesse caso 3)
+
 const IWantItThatWay = {
-    songName: 'I Want It That Way',
+    // atributos dos obejtos 
+    songName: 'I Want It That Way', 
     artist: 'Backstreet boys',
     file:'Backs',
     Liked: true,
@@ -36,17 +40,23 @@ const SoldaMeiaNoite = {
     Liked: true,
 
 }
-
+// funcoes auxiliares 
 let isPlaying = false
 let index = 0;
 let isShuffled = false
 let isLike = true
 
+
+// criamos um  constante de array pra  fazer as chamadas dentro da funcoes 
+// usamos o json armazenar em um local na nuvem e caso nao estivess mais na nuvem , colocamos um valor padrao 
 const playlist = JSON.parse(localStorage.getItem('playlist')) ?? [IWantItThatWay,ZumZumZum,SoldaMeiaNoite]  // array original
 
 let sortedPlaylist = [...playlist] //criando um clone para não alterar na original,com isso consigo brincar com essa, sem alterar o array original 
 
+
+// funcao de preenchimento do bota de like
 var LikeButtonRender = ()=>{
+    // fizemos uma condicao para caso de ser faso ou verdade, se for falso ele vai executar tal tarefa
   if (sortedPlaylist[index].Liked === false){
         LikeButton.querySelector('.bi').classList.remove('bi-heart')
         LikeButton.querySelector('.bi').classList.add('bi-heart-fill')
@@ -60,6 +70,20 @@ var LikeButtonRender = ()=>{
     
   }
 }
+// funcao que decide se cada audio ja ta ou nao marcado , caso nao ele executa a funcao likebuttonRender
+function LikeButtonClicked(){
+    if(sortedPlaylist[index].Liked === true){
+        sortedPlaylist[index].Liked =false
+        
+
+    }else{
+    sortedPlaylist[index].Liked = true
+   
+    }
+    LikeButtonRender();
+    //cria um local na nuvem com passa de prmiero do nome e depois o objet
+    localStorage.setItem('playlist', JSON.stringify(playlist))
+ }
         
 
 function playSong (){
@@ -68,19 +92,22 @@ function playSong (){
         play.querySelector('.bi').classList.remove('bi-play-circle-fill')
         play.querySelector('.bi').classList.add('bi-pause-circle-fill')
          song.play(); //evento de tocar
-            //criad
+            //tag auxiliar
          isPlaying = true;
     
 }
 function pauseSong (){
-   
+   //pause de  musica 
+   // pedido pra troca a o icon de (play) para (pause)
     play.querySelector('.bi').classList.remove('bi-pause-circle-fill')
     play.querySelector('.bi').classList.add('bi-play-circle-fill')
      song.pause();
      isPlaying = false;
+     //tag auxuliar
      
 
 }
+// funcao que decide se toca ou pausa
 function PlaypauseDecider(){
     if(isPlaying === true){
         pauseSong();
@@ -92,15 +119,18 @@ function PlaypauseDecider(){
 
 }
 
+// para carregar os elementos de imagem , audio, nome da banda e informações da audio
 function initializeSong (){
     coverDisc.src = `src/imagens/${sortedPlaylist[index].file}.jpg`;
 
     song.src = `src/Songs/${sortedPlaylist[index].file}.mp3`;
     songName.innerHTML = `${sortedPlaylist[index].songName}`
     bandName.innerHTML = sortedPlaylist[index].artist;
+    
     LikeButtonRender(); //inicia por padrao
 }
 
+// funcao de voltar pra musica interior 
 function previousSong(){
     if(index === 0){
         index = sortedPlaylist.length -1// quando chegar no -1 , ele vai jogar pro comprimento do array que nesse caso é o numero 2 
@@ -109,11 +139,14 @@ function previousSong(){
         index--
        
     }
+    /* pra musicaser executada e inicia os elementos */
     initializeSong();
     playSong();
    
   
 }
+
+// funcao de avançar 
 function nextSong(){
     if(index === sortedPlaylist.length -1){
         index = 0
@@ -123,19 +156,15 @@ function nextSong(){
         index++
        
     }
+    /* pra musica ser executada e inicia os elementos */
     initializeSong();
     playSong();
   
 }
-function updateProgress(){
-   /* song.currentTime  temo atual da musica
-    song.duration duração do som */
-    const barWidht = (song.currentTime/song.duration)*100;
 
-    currentProgress.style.setProperty('--progress', `${barWidht}%`);
-    SongTime.innerHTML = toHHMMSS(song.currentTime);
-}
 
+
+ // funcao de clicar na barra de progresso 
 function jumpTo (event){
   const widht =  progressContainer.clientWidth//clientWidth é a largura  da barrra de progresso total
   const clickPosition = event.offsetX // o tamanho em pixels da largura da onde foi clicada a barra de progresso
@@ -222,6 +251,20 @@ function nextOrRepeat(){
     }
 
 }
+// funcao da barra de progresso 
+function updateProgress(){
+    /* song.currentTime  temo atual da musica
+     song.duration duração do som */
+     // faz a matematica pra que aconteça a progressao da barra de progresso 
+     const barWidht = (song.currentTime/song.duration)*100;
+  // currentprogresss faaz a alteração da barra de progresso e faz ligação com o css
+     currentProgress.style.setProperty('--progress', `${barWidht}%`);
+    // faz lifacao com o outra funcao que faz a matematica do tempo
+ 
+    // ele apssa um paramentro pra funcao de TOhhmmss 
+     SongTime.innerHTML = toHHMMSS(song.currentTime);
+ }
+// funcao que faz  a matematica para as exibição posterior do tempo de exibição do audio
 function toHHMMSS(originalNumber){
     let hours = Math.floor(originalNumber/3600)
     let min = Math.floor((originalNumber - hours * 3600)/60)
@@ -229,25 +272,16 @@ function toHHMMSS(originalNumber){
    return `${hours.toString().padStart(2,'0')}:${min.toString().padStart(2,'0')}:${seconds.toString().padStart(2,'0')}`
 }
 
+// funcao que mostra o duracao total da musica
 function updateTotalTime(){
     
     totalTime.innerHTML = toHHMMSS(song.duration);
 }
-function LikeButtonClicked(){
-    if(sortedPlaylist[index].Liked === true){
-        sortedPlaylist[index].Liked =false
-        
 
-    }else{
-    sortedPlaylist[index].Liked = true
-   
-    }
-    LikeButtonRender();
-    localStorage.setItem('playlist', JSON.stringify(playlist))
- }
 
 
 initializeSong();
+//sessao de eventos do projeto 
 
 play.addEventListener('click',PlaypauseDecider);
 
